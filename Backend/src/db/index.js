46ecbd8e387592a141194c40be5940437f7f6e81
Zuned khan <zunedkhan107@gmail.com>
2026-02-09 22:02:47 +0530

    feat: add cookie-parser and cors middleware, implement error handling classes, and refactor app structure

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -4,7 +4,7 @@ import {DB_NAME} from "../constants.js"
 const connect_DB = async() => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`\n ✅ MongoDB connected !! DB HOST ${connectionInstance.connection.host}`);
+        console.log(` ✅ MongoDB connected !! DB HOST ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MongoDB connection Faied:", error);
         process.exit(1);
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,8 +1,13 @@
 import dotenv from "dotenv";
 import connect_DB from "./db/index.js";
+import {app} from "./app.js"
 
 dotenv.config({
     path: "./.env"
 });
 
 connect_DB()
+
+app.listen(process.env.PORT || 7000, () => {
+    console.log(` \n ✅ App is listening on port ${process.env.PORT}`);
+})
